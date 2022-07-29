6915ab42623f66bbc88d6cd3541fd274b2ac2511
Isabel <marisita2612@hotmail.com>
2022-07-29 18:00:10 -0500

    se crea form

---
+++ validacion.js
@@ -0,0 +1,26 @@
+// //Array Usuarios
+// const Users = [
+//     {email: "Pikachu", password:"pikachu1234"},
+//     {email: "Bulbasaur", password:"bulbasaur1234"}
+// ];
+
+// //Funcion Validar
+// function validarLogin() {
+//     let email = document.getElementById("usuario").value
+//     let password = document.getElementById("contraseña").value
+
+//     for (let i = 0; i < Users.length; i++) {
+//         if (email=="" || password=="") {
+//            console.log("los campos son obligatorios...por favor digite los campos ")
+//         }else{
+//             if (email==Users.email[i] && password == Users.password[i]) {
+//                 window.location.href ="index.html"
+//             }else{
+//                 alert ("lo siento no estas en la base de datos ");
+//             }
+//         }
+       
+//     }
+   
+// }
+
